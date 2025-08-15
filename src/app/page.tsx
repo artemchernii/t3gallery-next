3ecaba6172e16332317e8faccef1b22ce2758b6a
artemchernii <artemcherniy7@gmail.com>
2025-08-15 15:21:22 +0100

    feat: add dynamic config for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ const listOfImages = [
   id: index + 1,
 }));
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
 
